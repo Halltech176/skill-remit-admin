6632f67a58dfb9dc6c6139c94acfba19b3184382
Halltech176 <devhalltech@gmail.com>
2022-10-13 08:17:33 -0700

    commit6

--- src/Redux/Auth.jsx
+++ src/Redux/Auth.jsx
@@ -5,20 +5,21 @@ import { TOKEN } from "../../Api";
 import { User } from "../Redux/Actions";
 
 export const RequireAuth = ({ children }) => {
-  const navigate = useNavigate();
-  const dispatch = useDispatch();
-  const selector = useSelector((state) => state);
-  console.log(selector.user);
-  useEffect(() => {
-    dispatch(User());
-  }, []);
-  if (TOKEN !== undefined) {
-    return children;
-  } else {
-    useEffect(() => {
-      navigate("/login");
-    }, []);
-  }
+  //   const navigate = useNavigate();
+  //   const dispatch = useDispatch();
+  //   const selector = useSelector((state) => state);
+  //   console.log(selector.user);
+  //   useEffect(() => {
+  //     dispatch(User());
+  //   }, []);
+  //   if (TOKEN !== undefined) {
+  //     return children;
+  //   } else {
+  //     useEffect(() => {
+  //       navigate("/login");
+  //     }, []);
+  //   }
+  return children;
   //   adjkadsjk;
   console.log("yeah");
 };
